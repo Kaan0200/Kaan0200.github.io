e7156e0bb32161677044fda4d268e6cc54b04e34
A. Kaan Atesoglu <aka43@case.edu>
2020-11-15 01:29:01 -0500

    added vue and like some scaffolds

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<style>
+	body {
+		height: 100vh;
+		width: 100vw;
+		margin: 0;
+		padding: 0;
+		background-image: linear-gradient(#312F30, #393A41);
+	}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+
+
+
+		color: lightgrey;
+    }
+</style>
+
+<template>
+    <div id="app">
+		<Main></Main>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Main from './components/Main.vue';
+
+	@Component({
+		components: {
+			Main
+		}
+	})
+
+export default class App extends Vue {}
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="main-content">
+        <div>Welcome to my professional site</div>
+        <div>I'll put stuff here eventually</div>
+        <div>This site was created with Vue JS and not a whole lot else</div>
+
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Main extends Vue {}
+</script>
+<style>
+.main-content {
+    display: grid;
+    height: 100vh;
+    grid-template-rows: auto 1fr auto;
+}
+</style>
