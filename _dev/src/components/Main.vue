ac9638cbe2e63dc92f51985c6631312ee751b308
Kaan0200 <aka43@case.edu>
2020-11-28 12:52:22 -0500

    added simple displays

--- _dev/src/components/Main.vue
+++ _dev/src/components/Main.vue
@@ -3,9 +3,34 @@
         <div class="header-area">
             Welcome to my professional site
         </div>
-        <div>Coming soon...</div>
+        <div class="abilities">
+            <div class="section">
+                 <div style="font-size: 100px;">
+                    <i class='bx bxs-category-alt'></i>
+                </div>
+                <div>
+                    I understand a wide veriaty of technologies, topics, and skills.
+                </div>
+            </div>
+            <div class="section">
+                <div style="font-size: 100px;">
+                    <i class='bx bxs-brain'></i>
+                </div>
+                <div>
+                    I enjoy numerous programming projects and puzzles and love to try to familiarize many new technologies.  Mostly because it's a good brain workout!
+                </div>
+            </div>
+            <div class="section">
+                <div style="font-size: 100px;">
+                    <i class='bx bxs-network-chart'></i>
+                </div>
+                <div>
+                    I relish in testing my skills and knowledge through freelance work, developing, discussing, and creating solutions! 
+                </div>
+            </div>
+        </div>
         <div class="footer-area">
-            This site was created with Vue JS and not a whole lot else
+            This site was created with Vue JS and written, styled, and designed by Alan Atesoglu
         </div>
 
     </div>
@@ -17,6 +42,14 @@ import { Component, Vue } from "vue-property-decorator";
 export default class Main extends Vue {}
 </script>
 <style>
+.abilities {
+    display: flex;
+    align-content: center;
+    margin: auto;
+}
+.section {
+    width: 200px;
+}
 .header-area {
     margin: 1em;
 }
