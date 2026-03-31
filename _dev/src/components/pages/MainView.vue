d03f156b4dc9f4247d2ecdce991d6e34ec5fb820
A. Kaan Atesoglu <aka43@case.edu>
2026-03-31 01:01:08 -0400

    restarting project with lit web components

--- _dev/src/components/pages/MainView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        Main View Main View Main View Main View
-    </div>
-</template>
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-export default class LinksView extends Vue {}
-</script>
